6f9881bfc89c6f523828426071862f5db857b223
AakashSahani <sahaniaakash120@gmail.com>
2023-03-16 22:26:55 -0400

    Added pattern checks for bold and italics

--- app.js
+++ app.js
@@ -2,48 +2,65 @@
 const inputArea = document.getElementById('markdownInput');
 const markdown = document.getElementById('markdown');
 
+// Event Listeners
 inputArea.addEventListener('keyup', (e) => {
-	handleInput(e.currentTarget.value);
+	let text = e.currentTarget.value;
+	handleInput(text, pattern);
 });
 
 inputArea.addEventListener('change', (e) => {
-	console.log(e.value);
+	let text = e.currentTarget.value;
+	handleInput(text, pattern);
 });
 
-function handleInput(text) {
-	// Check for regex patterns
-	// RegEx for **string** pattern
-	const strongRegEx = new RegExp(/\*\*([^*]*)\*\*/g);
+// Regex patterns
+const strongRegEx = new RegExp(/\*\*([^*]*)\*\*/g);
 
-	// Check for <strong></strong> pattern
-	const matchWords = handleStrongPattern(text, strongRegEx);
+const italicRegEx = new RegExp(/\_([^_]*)\_/g);
 
-	// Assign it to markdown innerHtml
-	replaceMatchedWords(text, matchWords, strongRegEx);
+const headingOne = new RegExp(/^(\#{1}\s)/g);
+
+// Pattern Array
+const pattern = [
+	{ key: /\*\*/g, tag: '</strong>', reg: strongRegEx },
+	{ key: /\_/g, tag: '</em>', reg: italicRegEx },
+	// { key: /(\#{1}\s)/g, tag: '</h1>', reg: headingOne },
+];
+
+function handleInput(text, pattern) {
+	const matchWords = checkPattern(text, pattern);
+
+	matchWords.length > 0
+		? (markdown.innerHTML = replaceMatchedWords(text, matchWords, pattern))
+		: (markdown.innerHTML = text);
 }
 
-function handleStrongPattern(text, pattern) {
-	if (text.match(pattern)) {
-		const matches = text.match(pattern);
-		const replaceByStrongTagArray = matches
-			.map((match) => match.replace(/\*\*/g, '</strong>'))
-			.map((match) => match.replace('/', ''));
-		return replaceByStrongTagArray;
-	} else {
-		return [];
-	}
+// Pure Pattern Checking Functions
+function checkPattern(text, patterns) {
+	let replacedWords = [];
+	patterns.map((pattern) => {
+		if (text.match(pattern.reg) != null) {
+			const matches = text.match(pattern.reg);
+			replacedWords = replacedWords.concat(
+				matches
+					.map((match) => match.replace(pattern.key, pattern.tag))
+					.map((match) => match.replace('/', ''))
+			);
+		}
+	});
+	console.log(replacedWords);
+	return replacedWords;
 }
 
-function replaceMatchedWords(text, matchedWords, pattern) {
+function replaceMatchedWords(text, matchedWords, patterns) {
 	let newWord = '';
-	if (text.match(/\*\*/)) {
-		matchedWords &&
-			matchedWords.map((words) => {
-				newWord += text.replace(pattern, words);
-			});
-	} else {
-		newWord = text;
-	}
-	console.log(newWord);
-	markdown.innerHTML = newWord;
+	patterns.map((pattern, index) => {
+		if (text.match(pattern.key)) {
+			matchedWords &&
+				matchedWords.map((words) => {
+					newWord = text.replace(pattern.reg, matchedWords[index]);
+				});
+		}
+	});
+	return newWord;
 }
